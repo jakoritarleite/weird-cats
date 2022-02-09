d834e0fe180ab451f7a27d6e0afe531b1332bb2d
Joao Koritar <jakoritarleite@gmail.com>
2022-02-09 12:08:41 -0300

    feat: create the NFT contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    uint256 public tokenLimit;
+
+    constructor(uint256 _tokenLimit) ERC721("weirdpixels", "WEIRDPIXELS") {
+        tokenLimit = _tokenLimit;
+        console.log("Deploying Weird Pixels contract.");
+    }
+
+    modifier checkLimit() {
+        require(_tokenIds.current() < tokenLimit, "Reached token limit");
+        _;
+    }
+
+    function getCurrentTokenId() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
+    function mintNFT(string memory metadataIpfsUri) public checkLimit {
+        uint256 tokenId = _tokenIds.current();
+
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, metadataIpfsUri);
+
+        console.log(
+            "An NFT w/ ID %s has been minted to %s",
+            tokenId,
+            msg.sender
+        );
+
+        _tokenIds.increment();
+    }
+}
